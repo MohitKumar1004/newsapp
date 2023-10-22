01799f8938906052e567455fed0df0cbcd522556
MohitKumar1004 <104577734+MohitKumar1004@users.noreply.github.com>
2023-10-22 10:23:13 +0530

    Project using Function Based Components Used Infinite Scroll, Top Loading, All Class Based Components, Env File with adding Sticky Navbar and fixing some bugs ,errors and warnings and image

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -4,9 +4,7 @@ import React from 'react'
 export default function Spinner() {
     return (
         <div className="container text-center p-3">
-            <div className="spinner-border" role="status">
-                {/* <span class="sr-only">Loading...</span> */}
-            </div>
+            <div className="spinner-border" role="status"></div>
         </div>
     )
 }
